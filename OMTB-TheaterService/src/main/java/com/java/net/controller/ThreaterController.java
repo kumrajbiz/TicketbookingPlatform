bb9e7af429b7f8930fbd4a6a59f1d38927f1ba18
kumrajbiz <126977951+kumrajbiz@users.noreply.github.com>
2023-03-09 11:41:50 +0530

    File uploaded

---
+++ OMTB-TheaterService/src/main/java/com/java/net/controller/ThreaterController.java
@@ -0,0 +1,37 @@
+package com.java.net.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.java.net.entity.Theater;
+import com.java.net.service.TheaterService;
+
+@Controller
+@RestController
+@RequestMapping("/theater")
+public class ThreaterController {
+	
+	@Autowired
+	private TheaterService theaterService;
+	
+	@PostMapping("/")
+	public ResponseEntity<String> createTheater(@RequestBody Theater theater ){
+		theaterService.createTheater(theater);
+        String responseMessage = "New Theater has been updated to system";
+        HttpStatus status = HttpStatus.ACCEPTED;
+        return ResponseEntity.status(status).body(responseMessage);
+	}
+	
+	@GetMapping("/{tId}")
+	public Theater getTheater(@PathVariable("tId") String theaterId) {
+		return theaterService.getTheater(theaterId);
+	}
+}
